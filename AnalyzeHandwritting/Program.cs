bf44b0571b2992e98fd3ade4bf297e194aab6d9b
Gisela Torres <giselatb@outlook.com>
2018-08-16 12:04:40 +0000

    Computer Vision API Demo

---
+++ AnalyzeHandwritting/Program.cs
@@ -0,0 +1,162 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AnalyzeHandwritting
+{
+    class Program
+    {
+        //API Reference
+        //https://northeurope.dev.cognitive.microsoft.com/docs/services/5adf991815e1060e6355ad44/operations/587f2c6a154055056008f200/console
+
+        static void Main()
+        {
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Handwriting Recognition:");            
+            Console.Write("Enter the path of the directory: ");
+            string folderPath = Console.ReadLine();
+            
+            if (Directory.Exists(folderPath))
+            {   
+                Parallel.ForEach(Directory.GetFiles(folderPath), new ParallelOptions { MaxDegreeOfParallelism = 4 }, (imagePath) =>
+                    {
+                        // Make the REST API call.
+                        Console.WriteLine($"\nAnalyzing {imagePath} \nWait a moment for the results to appear.{DateTime.Now.ToLongTimeString()}\n");
+                        ReadHandwrittenText(imagePath).Wait();                        
+                    });
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Gets the handwritten text from the specified image file by using
+        /// the Computer Vision REST API.
+        /// </summary>
+        /// <param name="imageFilePath">The image file with handwritten text.</param>
+        static async Task ReadHandwrittenText(string imageFilePath)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ConfigurationManager.AppSettings["ComputerVisionKey"]);
+
+                // Request parameter.
+                // Note: The request parameter changed for APIv2.
+                // For APIv1, it is "handwriting=true".
+                string requestParameters = "mode=Handwritten";
+
+                // Assemble the URI for the REST API Call.
+                string uri = ConfigurationManager.AppSettings["UriBase"] + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                // Two REST API calls are required to extract handwritten text.
+                // One call to submit the image for processing, the other call
+                // to retrieve the text found in the image.
+                // operationLocation stores the REST API location to call to
+                // retrieve the text.
+                string operationLocation;
+
+                // Request body.
+                // Posts a locally stored JPEG image.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses content type "application/octet-stream".
+                    // The other content types you can use are "application/json"
+                    // and "multipart/form-data".
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // The first REST call starts the async process to analyze the
+                    // written text in the image.
+                    response = await client.PostAsync(uri, content);
+                }
+
+                // The response contains the URI to retrieve the result of the process.
+                if (response.IsSuccessStatusCode)
+                    operationLocation = response.Headers.GetValues("Operation-Location").FirstOrDefault();
+                else
+                {
+                    // Display the JSON error data.
+                    string errorString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("\n\nResponse:\n{0}\n", JToken.Parse(errorString).ToString());
+                    var jsonError = JToken.Parse(errorString);
+
+                    //Hints
+                    //Supported image formats: JPEG, PNG and BMP.
+                    //Image file size must be less than 4MB.
+                    //Image dimensions must be at least 40 x 40, at most 3200 x 3200.
+                    File.WriteAllText($"{Path.GetDirectoryName(imageFilePath)}\\{Path.GetFileNameWithoutExtension(imageFilePath)}-ERROR-{jsonError["error"]["code"].ToString()}.json", errorString);
+                    return;
+                }
+
+                // The second REST call retrieves the text written in the image.
+                //
+                // Note: The response may not be immediately available. Handwriting
+                // recognition is an async operation that can take a variable amount
+                // of time depending on the length of the handwritten text. You may
+                // need to wait or retry this operation.
+                //
+                // This example checks once per second for ten seconds.
+                string contentString;
+                int i = 0;
+                do
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    response = await client.GetAsync(operationLocation);
+                    contentString = await response.Content.ReadAsStringAsync();
+                    ++i;
+                }
+                while (i < 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1);
+
+                if (i == 10 && contentString.IndexOf("\"status\":\"Succeeded\"") == -1)
+                {
+                    Console.WriteLine("\nTimeout error.\n");
+                    File.WriteAllText($"{Path.GetDirectoryName(imageFilePath)}\\{Path.GetFileNameWithoutExtension(imageFilePath)}-TIMEOUT.json", "TIMEOUT");
+                    return;
+                }
+
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n\n{0}\n", JToken.Parse(contentString).ToString());
+                //And save the result into a JSON
+                string json = JsonConvert.SerializeObject(contentString);
+                File.WriteAllText($"{Path.GetDirectoryName(imageFilePath)}\\{Path.GetFileNameWithoutExtension(imageFilePath)}.json", json);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
+        }
+    }
+}
